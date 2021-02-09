17295cbd08d1828d92d91f336b43616507ac114a
Oliver Brandt <oliver.brandt@hamburg.de>
2021-02-09 21:33:51 +0100

    Move version to required_providers

--- resource-group/main.tf
+++ resource-group/main.tf
@@ -1,4 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.12.0"
+    }
+  }
+}
+
 provider "azurerm" {
-  version = "2.12.0"
   features {}
 }
